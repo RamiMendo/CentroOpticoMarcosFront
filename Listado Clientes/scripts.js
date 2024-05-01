b2296761d86d402352ea9925809bb9a24b6d7a9d
Ramiro Mendoza <ramamendoza@gmail.com>
2024-04-30 23:47:36 -0300

    Primer commit del proyecto

---
+++ Listado Clientes/scripts.js
@@ -0,0 +1,243 @@
+const config ={
+    modoCreacion: true,
+    idModificar: null
+};
+
+const cliente =[
+    {
+        nombre: 'Conor',
+        telefono: 11122121,
+        direccion: 'Avenida Juan B Justo 4000'
+    },
+    {
+        nombre: 'Pedro',
+        telefono: 14234121,
+        direccion: 'Avenida De Los Corrales 4000'
+    }
+]
+
+function modalCliente(){
+
+    const modal = document.createElement('div');
+    modal.id = "container-modal";
+
+    modal.innerHTML = `
+        <h1 id="titulo-modal">Cliente</h1>
+        <form id="form-modal">
+            <label class="label-modal" id="label-nombre-modal">Nombre</label>
+            <input class="input-modal" id="input-nombre-modal" type="text" placeholder="Conor McGregor">
+            <span id="span-nombre" class="error">* El nombre es obligatorio</span>
+            <label class="label-modal" id="label-telefono-modal">Teléfono</label>
+            <input class="input-modal" id="input-telefono-modal" type="number" placeholder="1122222222">
+            <span id="span-telefono" class="error">* El telefono es obligatorio</span>
+            <label class="label-modal" id="label-direccion-modal">Dirección</label>
+            <input class="input-modal" id="input-direccion-modal" type="text" placeholder="Calle Falsa 123">
+            <span id="span-direccion" class="error">* La direccion es obligatoria</span>
+        </form>
+    `;
+
+    const botonAceptar = document.createElement('button');
+    botonAceptar.className="boton-modal";
+    botonAceptar.id="boton-modal-aceptar";
+    botonAceptar.textContent="ACEPTAR";
+    botonAceptar.addEventListener('click', () =>{
+        const nombre = document.getElementById('input-nombre-modal');
+        nombre.addEventListener('input', e =>{
+            //console.log('Sibling', e.target.nextSibling);
+            //console.log('Sibling', e.target.nextSibling.nextSibling);
+
+            //console.log('Parent', e.target.parentNode);
+            //console.log('Error', e.target.parentNode.querySelector('.error'));
+
+            // console.log('Parent', e.target.closest('div').querySelector('.error'));
+
+            const span = e.target.closest('div').querySelector('.error');
+            
+            if(nombre.value === ''){
+                span.style.color = 'rgb(255, 87, 87)';
+            }else{
+                span.style.color = '#3a632e';
+            }
+
+        });
+
+        const telefono = document.getElementById('input-telefono-modal');
+        telefono.addEventListener('input', e =>{
+            const span = e.target.closest('div').querySelector('.error');
+
+            if(telefono.value === ''){
+                span.style.color = 'rgb(255, 87, 87)';
+            }else{
+                span.style.color = '#3a632e';
+            }
+            
+            
+            // let mensaje = 'El telefono es obligatorio';
+
+            // if(telefono.checkVisibility()){
+            //     span.style.display = 'none';
+            // }else{
+            //     span.style.display = 'flex';
+            //     if(telefono.validity.badInput){
+            //         mensaje='Debe ingresar un numero';
+                // }else if(telefono.validity.rangeUnderflow){
+                    
+                // }else if(telefono.validity.rangeOverflow){
+
+                // }else if(telefono.validity.typeMismatch){
+                //}
+                // }else{
+                //     span.textContent = 'El telefono es obligatorio';
+                // }
+            //}
+
+            //span.textContent = mensaje;
+        });
+
+        const direccion = document.getElementById('input-direccion-modal');
+        direccion.addEventListener('input', e =>{
+
+            const span = e.target.closest('div').querySelector('.error');
+
+            if(direccion.value === ''){
+                span.style.color = 'rgb(255, 87, 87)';
+            }else{
+                span.style.color = '#3a632e';
+            }
+
+            //para campos como el email:
+
+            // const valida= regexEmail.test(email.value);
+
+        });
+
+        if(config.modoCreacion){ 
+            cliente.push({
+                nombre:nombre.value,
+                telefono:telefono.value,
+                direccion:direccion.value
+            });
+        }else{
+            cliente[config.idModificar].nombre= nombre.value;
+            cliente[config.idModificar].telefono= telefono.value;
+            cliente[config.idModificar].direccion= direccion.value;
+        }
+
+        modal.style.display='none';
+
+        muestraClientes();
+
+    });
+
+    const botonCerrar = document.createElement('button');
+    botonCerrar.className="boton-modal";
+    botonCerrar.id="boton-modal-cerrar";
+    botonCerrar.textContent="X";
+    botonCerrar.addEventListener('click', () =>{
+        modal.style.display = 'none';
+    });
+
+    modal.append(botonAceptar, botonCerrar);
+    document.body.appendChild(modal);
+
+}
+
+//************ CREA FILTRO */
+
+const inputFicha = document.getElementById('input-buscador');
+inputFicha.addEventListener('input', e =>{
+        muestraClientes();
+});    
+
+//************ MUESTRA CLIENTES */
+
+function muestraClientes(){
+
+    const fg = document.createDocumentFragment();
+
+    const tbody = document.getElementById('tbody-tabla');
+    tbody.addEventListener('click', e=>{
+        const index = Number(e.target.parentNode.parentNode.parentNode.dataset.index);
+        if(e.target.parentNode.nodeName === 'BUTTON'){
+            if(e.target.parentNode.dataset.type === 'delete'){
+                handleDelete(index);
+            }else{
+                handleEdit(index);
+            }
+        }
+    });
+    tbody.innerHTML=``;
+
+    cliente
+        .filter(p => {
+            return (p.nombre.toLowerCase().includes(inputFicha.value));
+        })
+
+        .forEach((p,index)=>{
+            const trCuerpo = document.createElement('tr');
+            trCuerpo.dataset.index = index;
+            trCuerpo.className="tr-cuerpo";
+            trCuerpo.innerHTML=`
+                <td class="td-nombre input-td">${p.nombre}</td>
+                <td class="td-telefono input-td">${p.telefono}</td>
+                <td class="td-direccion input-td">${p.direccion}</td>
+            `;
+            
+            //************ EDICION */
+
+            const tdEdicion = document.createElement('td');
+            tdEdicion.className="td-edicion";
+
+            const botonEdicion = document.createElement('button');
+            botonEdicion.className="boton-edicion boton-tabla";
+            botonEdicion.dataset.type='edit';
+            botonEdicion.innerHTML=`<img src="images/icons8-editar-48.png" alt="" class="imagen-editar">`;
+
+            tdEdicion.append(botonEdicion);
+
+            //************ ELIMINAR */
+
+            const tdEliminar = document.createElement('td');
+            tdEliminar.className="td-eliminar";
+            
+            const botonEliminar = document.createElement('button');
+            botonEliminar.className="boton-eliminar boton-tabla";
+            botonEliminar.dataset.type='delete';
+            botonEliminar.innerHTML=`<img src="images/icons8-basura-48.png" alt="" class="imagen-eliminar">`;
+
+            tdEliminar.append(botonEliminar);
+            trCuerpo.append(tdEdicion,tdEliminar);
+            fg.append(trCuerpo);
+            
+        });
+
+        tbody.append(fg);
+    
+    table.append(tbody);
+
+}
+
+modalCliente();
+muestraClientes();
+
+function handleDelete(index){
+
+    cliente.splice(index,1);
+    muestraClientes();
+}
+
+function handleEdit(index){
+    const modal = document.getElementById('container-modal');
+    const nombre = document.getElementById('input-nombre-modal');
+    const telefono = document.getElementById('input-telefono-modal');
+    const direccion = document.getElementById('input-direccion-modal');
+    
+    config.modoCreacion=false;
+    config.idModificar=index;
+
+    nombre.value=cliente[index].nombre;
+    telefono.value=cliente[index].telefono;
+    direccion.value=cliente[index].direccion;
+    
+    modal.style.display='flex';
+}
